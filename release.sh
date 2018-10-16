4cc9a966f0f8c4205df7e04738bf25e0514a59ca
Takara Hokao <takara.hokao@gmail.com>
2018-10-17 03:28:41 +0900

    be able to deploy binary

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+echo "Start release"
+
+gox -os="darwin linux windows" -arch="amd64 386" -output="$REPO_ROOT/dist/{{.Dir}}_{{.OS}}_{{.Arch}}" ./cmd/dtdiff
+ghr -u tkrtmy -t $GITHUB_TOKEN --replace `grep 'Version =' ./cmd/dtdiff/version.txt | sed -E 's/.*"(.+)"$$/\1/'` dist/
+
+echo "Completed"
